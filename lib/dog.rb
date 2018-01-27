20d321cf8b3e8eb26197abca3ab114c98d034625
jencekade <gaephuoc@gmail.com>
2018-01-27 03:23:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 # Your code goes here!
 class Dog
   def initialize(name)
-
+    @name = name
   end
 
-  def @name = name
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
   end
 
   def bark
